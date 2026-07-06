11be4d910fc2aa5e7a647fc1174e9dcfe54b8c47
arianasuha <arianahoque94@gmail.com>
2026-07-06 13:15:48 +0600

    fixed the drf suffix issue

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from auth_api.views import LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("server-api/auth-api/", include("auth_api.urls")),
+    path("server-api/category-api/", include("equipment_api.urls")),
     
-    path('api/auth/', include('auth_api.urls')),  
-    path('api/auth/login/', LoginView.as_view(), name='token_obtain_pair'),
-    # path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('swagger-api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger-api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
