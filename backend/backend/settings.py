0a255cb46c9efbe84d60929cc1b35fc1c5cc9182
arianasuha <arianahoque94@gmail.com>
2026-08-08 15:37:41 +0600

    Increased the access token lifetime for testing purpose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -210,7 +210,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     "LEEWAY": timedelta(seconds=10),
     
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
