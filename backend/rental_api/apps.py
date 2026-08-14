c9d30fa6f18a77545e94e3fdb20addf8805d5a70
arianasuha <arianahoque94@gmail.com>
2026-08-14 21:18:37 +0600

    Changed app name

--- backend/rental_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EquipmentApiConfig(AppConfig):
-    name = "rental_api"
--- backend/rental_api/paginations.py
+++
@@ -1,5 +0,0 @@
-from backend.paginations import CustomPagination
-
-class EquipmentPagination(CustomPagination):
-    page_size = 4
-    
